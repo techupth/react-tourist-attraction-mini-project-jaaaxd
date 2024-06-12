61bf148a26a5adbc6238267b1dfcf112ec6286e3
Ja <ja.techup@gmail.com>
2024-06-12 21:40:26 +0700

    refactor: create separated components by using props

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,5 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import PostList from "../components/PostList";
+import Header from "../components/Header";
 
 function Homepage() {
   const [posts, setPosts] = useState([]);
@@ -16,123 +18,22 @@ function Homepage() {
     getPosts();
   }, [searchInput]);
 
-  const textLimit = (description) => {
-    const maxChars = 100;
-    return description.length > maxChars
-      ? description.substring(0, maxChars) + " ... "
-      : description;
-  };
-
-  const additionalPhotos = (photos) => {
-    const photosExceptFirst = [...photos];
-    photosExceptFirst.shift();
-    return photosExceptFirst.map((item, index) => {
-      return <img key={index} src={item} />;
-    });
-  };
-
-  const renderCategory = (tags) => {
-    const tagsExceptLast = [...tags];
-    const lastTag = tagsExceptLast.pop();
-
-    const handleTagClick = (tag) => {
-      if (searchInput.includes(tag)) {
-        return;
-      } else {
-        setSearchInput(searchInput ? `${searchInput} ${tag}` : tag);
-      }
-    };
-
-    return (
-      <ul>
-        {tagsExceptLast.map((item, index) => (
-          <li
-            onClick={() => {
-              handleTagClick(item);
-            }}
-            key={index}
-          >
-            {item}
-          </li>
-        ))}
-        และ
-        <li
-          onClick={() => {
-            handleTagClick(lastTag);
-          }}
-        >
-          {lastTag}
-        </li>
-      </ul>
-    );
-  };
-
   return (
     <div>
       <style>
         @import
         url('https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
       </style>
-      <h1
-        className="main-title"
-        onClick={() => {
-          setSearchInput("");
-        }}
-      >
-        เที่ยวไหนดี
-      </h1>
-      <div className="search-box">
-        <p className="search-label">ค้นหาที่เที่ยว</p>
-        <input
-          type="text"
-          value={searchInput}
-          onChange={(event) => {
-            setSearchInput(event.target.value);
-          }}
-        />
-      </div>
-      <div className="section">
-        <div className="section-message">หาที่เที่ยวแล้วไปกัน ...</div>
-        <div className="result-count">Results ({posts.length})</div>
-      </div>
-      <div className="post-list">
-        {posts.map((post, index) => {
-          return (
-            <div className="post-item" key={index}>
-              <img className="main-image" src={post.photos[0]} />
-              <div className="post-details">
-                <h4
-                  className="post-title"
-                  onClick={() => {
-                    window.open(post.url, "_blank");
-                  }}
-                >
-                  {post.title}
-                </h4>
-                <p className="post-desc" id="post-desc">
-                  {textLimit(post.description)}
-                  <a href={post.url} target="_blank">
-                    อ่านต่อ
-                  </a>
-                </p>
-                <div className="category">หมวด {renderCategory(post.tags)}</div>
-                <div className="post-footer">
-                  <div className="additional-photo">
-                    {additionalPhotos(post.photos)}
-                  </div>
-                  <img
-                    className="share-button"
-                    src="https://cdn-icons-png.freepik.com/512/6994/6994770.png"
-                    onClick={() => {
-                      navigator.clipboard.writeText(post.url);
-                    }}
-                  />
-                </div>
-              </div>
-            </div>
-          );
-        })}
-      </div>
+      <Header
+        posts={posts}
+        searchInput={searchInput}
+        setSearchInput={setSearchInput}
+      />
+      <PostList
+        posts={posts}
+        searchInput={searchInput}
+        setSearchInput={setSearchInput}
+      />
     </div>
   );
 }
